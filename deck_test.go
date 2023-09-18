1aeadf983afebfabc635382cf9abc1d1307b25fa
sourav2831 <sourav2831@gmail.com>
2023-09-18 12:41:44 +0530

    added test for IO files

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d:= newDeck();
@@ -12,4 +15,18 @@ func TestNewDeck(t *testing.T) {
 	if d[0]!= "Ace of Spades"{
 		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_deckTesting")
+
+	deck:= newDeck()
+	deck.saveToFile("_deckTesting")
+	loadedDeck:= newDeckFromFile("_deckTesting")
+
+	if len(loadedDeck)!=16{
+		t.Errorf("Expected deck length of 16, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_deckTesting")
 }
