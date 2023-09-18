510d8575d60727fdae5278097b9d92a1107fce2f
sourav2831 <sourav2831@gmail.com>
2023-09-18 17:33:24 +0530

    added structs

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName string
+}
+
 func main(){
 	//var variableName string="Hello world";
 	// variableName:=getVariableName()
 	// fmt.Println(variableName)
 	//slice()
-	cards:=newDeck()
+	//cards:=newDeck()
 	// hand,remainingCards:=deal(cards,5)
 	// hand.print()
 	// remainingCards.print()
@@ -13,7 +20,12 @@ func main(){
 	// cards.saveToFile("deck")
 	// cards := newDeckFromFile("deck")
 	// cards.shuffle()
-	cards.print()
+	//cards.print()
+	bob:= person{
+		firstName: "bob",
+		lastName: "willy",
+	}
+	fmt.Printf("%+v",bob)
 }
 
 func getVariableName() string {
